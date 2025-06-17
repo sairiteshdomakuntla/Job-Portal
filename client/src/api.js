d8b841d852f1cc594fffa953f731629e1d44e95e
Sai Ritesh <sairiteshdomakuntla@gmail.com>
2025-06-17 09:24:23 +0530

    Endpoint Changed

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/jobs',
+  // baseURL: 'http://localhost:5000/jobs',
+    baseURL: 'https://job-portal-crud.onrender.com/jobs',
+
 });
 
 export default api;
